adb41714a0d3c79caccafcd43aef3263a8fa8d25
Ryan Jowers <rjowers@g.clemson.edu>
2017-02-01 14:49:19 -0500

    finished question 4 of hardmode

--- hardmode.js
+++ hardmode.js
@@ -1,12 +1,12 @@
 // Question 1
 
-var numbers = [1,12,4,18,9,7,11,3,101,5,6];
+var numbers = [1, 12, 4, 18, 9, 7, 11, 3, 101, 5, 6];
 
-var largest= 0;
+var largest = 0;
 
-for (var i=0; i<=largest;i++){
-    if (numbers[i]>largest) {
-        var largest=numbers[i];
+for (var i = 0; i <= largest; i++) {
+    if (numbers[i] > largest) {
+        var largest = numbers[i];
     }
 }
 
@@ -14,13 +14,13 @@ console.log(largest)
 
 //Question 2
 
-var strings = ['this','is','a','collection','of','words'];
-var lgth= 0;
+var strings = ['this', 'is', 'a', 'collection', 'of', 'words'];
+var lgth = 0;
 var longest;
 
 
-for(var i=0; i < strings.length; i++){
-    if(strings[i].length > lgth){
+for (var i = 0; i < strings.length; i++) {
+    if (strings[i].length > lgth) {
         var lgth = strings[i].length;
         longest = strings[i];
     }
@@ -32,15 +32,31 @@ console.print(longest)
 //Question 3
 
 
+var numbers = [1, 12, 4, 18, 9, 7, 11, 3, 101, 5, 6];
 
-
-
-var numbers = [1,12,4,18,9,7,11,3,101,5,6];
-
-var even = function (num) {
-return num % 2 === 0;
+var even = function(num) {
+    return num % 2 === 0;
 };
 
 var numbers = numbers.filter(even);
 
 console.log(numbers);
+
+///////////////////////Question 4
+
+var numbers = [1, 12, 4, 18, 9, 7, 11, 3, 101, 5, 6];
+
+function odd(numbers) {
+
+    if (numbers % 2 == 1) {
+          return true
+    }
+    else {
+      return false;
+    }
+};
+
+var alltheodds = numbers.filter(odd)
+
+
+console.log(alltheodds);
