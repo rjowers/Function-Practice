73bfe522665855346fc6553e92c0b22a491a50bf
Ryan Jowers <rjowers@g.clemson.edu>
2017-02-01 16:06:11 -0500

    adding html.index and for loop to question 4

--- hardmode.js
+++ hardmode.js
@@ -4,7 +4,7 @@ var numbers = [1, 12, 4, 18, 9, 7, 11, 3, 101, 5, 6];
 
 var largest = 0;
 
-for (var i = 0; i <= largest; i++) {
+for (var i = 0; i <= numbers.length; i++) {
     if (numbers[i] > largest) {
         var largest = numbers[i];
     }
@@ -15,18 +15,16 @@ console.log(largest)
 //Question 2
 
 var strings = ['this', 'is', 'a', 'collection', 'of', 'words'];
-var lgth = 0;
-var longest;
+var longest = "";
 
 
 for (var i = 0; i < strings.length; i++) {
-    if (strings[i].length > lgth) {
-        var lgth = strings[i].length;
+    if (strings[i].length > longest.length) {
         longest = strings[i];
     }
 }
 
-console.print(longest)
+console.log(longest)
 
 
 //Question 3
@@ -44,19 +42,56 @@ console.log(numbers);
 
 ///////////////////////Question 4
 
+// TODO write question 4 with a for loop and a conditional instead of filter
+
 var numbers = [1, 12, 4, 18, 9, 7, 11, 3, 101, 5, 6];
 
 function odd(numbers) {
-
-    if (numbers % 2 == 1) {
-          return true
-    }
-    else {
-      return false;
-    }
+  return numbers % 2 === 1;
+    // if (numbers % 2 === 1) {
+    //       return true
+    // }
+    // else {
+    //   return false;
+    // }
 };
 
 var alltheodds = numbers.filter(odd)
 
 
 console.log(alltheodds);
+
+// for loop
+
+var numbers = [1, 12, 4, 18, 9, 7, 11, 3, 101, 5, 6];
+var odd = []
+
+    for (var i = 0; i < numbers.length; i++) {
+
+        if ((numbers[i] % 2) === 1) {
+            odd.push(numbers[i]);
+
+        }
+
+};
+
+console.log(odd);
+
+
+//Question 5
+
+
+
+var strings = ['this','is','a','collection','of','words'];
+
+
+
+function checkforis(strings) {
+
+    return strings === "is";
+};
+
+
+var alltheis = strings.find(checkforis)
+
+console.log (alltheis);
