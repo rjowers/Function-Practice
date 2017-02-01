8c5b9a0c42027f92676014e28d747eda26b2779a
Ryan Jowers <rjowers@g.clemson.edu>
2017-02-01 13:56:03 -0500

    finsihed question 1 and 2 on hardmode

--- hardmode.js
+++ hardmode.js
@@ -1,1 +1,29 @@
 // Question 1
+
+var numbers = [1,12,4,18,9,7,11,3,101,5,6];
+
+var largest= 0;
+
+for (var i=0; i<=largest;i++){
+    if (numbers[i]>largest) {
+        var largest=numbers[i];
+    }
+}
+
+console.log(largest)
+
+//Question 2
+
+var strings = ['this','is','a','collection','of','words'];
+var lgth= 0;
+var longest;
+
+
+for(var i=0; i < strings.length; i++){
+    if(strings[i].length > lgth){
+        var lgth = strings[i].length;
+        longest = strings[i];
+    }
+}
+
+console.print(longest)
